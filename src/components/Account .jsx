cf835b46618e61be76e304197dd4f391c089b846
mrazadtamal <mrazadt@gmail.com>
2022-01-19 21:22:18 +0600

    home

--- src/components/Account .jsx
+++ src/components/Account .jsx
@@ -5,7 +5,7 @@ const Account = () => {
   return (
     <div className="account">
       <span className="material-icons-outlined" title="Account">
-        account_circle
+        circle
       </span>
       <a href="signup.html">Signup</a>
     </div>
---
+++ src/components/Video.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Video = () => {
+  return (
+    <>
+      <a href="quiz.html">
+        <div className="video">
+          <img src={""} alt="Video Title" />
+          <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+          <div className="qmeta">
+            <p>10 Questions</p>
+            <p>Score : Not taken yet</p>
+          </div>
+        </div>
+      </a>
+    </>
+  );
+};
+
+export default Video;
