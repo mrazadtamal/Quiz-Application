fb3344061183041d920232aeb1ec324acbd02e8a
mrazadtamal <mrazadt@gmail.com>
2022-01-20 23:15:31 +0600

    quiz

---
+++ src/components/pages/Quiz.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Quiz = () => {
+  return (
+    <>
+      <h1>Pick three of your favorite Star Wars Flims</h1>
+      <h4>Question can have multiple answers</h4>
+      {/* <Answers />
+      <ProgressBar />
+      <MiniPlayer /> */}
+    </>
+  );
+};
+
+export default Quiz;
