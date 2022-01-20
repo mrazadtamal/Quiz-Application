933df5568a69b28fc13888c0724cebe846c812a6
mrazadtamal <mrazadt@gmail.com>
2022-01-20 16:17:21 +0600

    login

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,9 +2,38 @@ import React from "react";
 
 const Signup = () => {
   return (
-    <div>
+    <>
       <h1>Create an account</h1>
-    </div>
+
+      <div className="column">
+        <Illustration />
+        <Form className={`${classes.signup}`}>
+          <TextInput type="text" placeholder="Enter name" icon="person" />
+
+          <TextInput
+            type="text"
+            placeholder="Enter email"
+            icon="alternate_email"
+          />
+
+          <TextInput type="password" placeholder="Enter password" icon="lock" />
+
+          <TextInput
+            type="password"
+            placeholder="Confirm password"
+            icon="lock_clock"
+          />
+
+          <Checkbox text="I agree to the Terms &amp; Conditions" />
+
+          <Button>Submit Now</Button>
+
+          <div className="info">
+            Already have an account? <a href="login.html">Login</a> instead.
+          </div>
+        </Form>
+      </div>
+    </>
   );
 };
 
