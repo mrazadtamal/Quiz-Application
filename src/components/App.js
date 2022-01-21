787b68f5c3d887e4f35db5589e86c86a18746a61
mrazadtamal <mrazadt@gmail.com>
2022-01-21 23:26:16 +0600

    quiz

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,17 @@
 import "../styles/App.css";
 import Layout from "./Layout";
 // import Home from "./pages/Home";
-import Signup from "./pages/Signup";
+// import Signup from "./pages/Signup";
 // import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 
 function App() {
   return (
     <Layout>
       {/* <Home /> */}
-      <Signup />
+      {/* <Signup /> */}
       {/* <Login /> */}
+      <Quiz />
     </Layout>
   );
 }
