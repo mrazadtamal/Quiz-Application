7d76f37f1518b5bfff2f20cdc9e4a423ccbb0a6f
mrazadtamal <mrazadt@gmail.com>
2022-01-23 21:45:29 +0600

    icon

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import classes from "../styles/Button.module.css";
-
-export default function Button({ children }) {
-  return (
-    <div className={classes.button}>
-      <span>{children}</span>
-    </div>
-  );
-}
--- src/components/Checkbox.jsx
+++
@@ -1,7 +0,0 @@
-export default function Checkbox({ text, ...rest }) {
-  return (
-    <label>
-      <input type="checkbox" {...rest} /> <span>{text}</span>
-    </label>
-  );
-}
