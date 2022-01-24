89cda9d6e4c04ab495c6eeeea816555d75dbfe70
mrazadtamal <mrazadt@gmail.com>
2022-01-24 17:02:27 +0600

    routers

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "../styles/App.css";
 import Layout from "./Layout";
 import Result from "./pages/Result";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
@@ -14,7 +13,6 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
           <Route path="/quiz" element={<Quiz />} />
