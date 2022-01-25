fbcc4f60a8c85e0dc921bdc8745647920cb996f1
mrazadtamal <mrazadt@gmail.com>
2022-01-25 11:48:57 +0600

    firebase

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,4 +1,5 @@
 import React, { useContext, useState } from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 import "../firebase";
 
@@ -11,6 +12,23 @@ export function useAuth() {
 export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
   const [correntUserr, setCorrentUserr] = useState();
+
+  // signup
+  function signup(email, password, username) {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+      });
+  }
+
   return (
     <AuthContext.Provider value={value}>
       {!loading && children}
