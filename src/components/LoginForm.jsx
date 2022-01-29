58213f57a4ada12ae4e4fa536adf8adc3c9cfe5d
mrazadtamal <mrazadt@gmail.com>
2022-01-29 23:25:00 +0600

    60%

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import Button from "./Button";
 import Form from "./Form";
@@ -11,7 +11,7 @@ export default function LoginForm() {
   const [error, setError] = useState();
   const [loading, setLoading] = useState();
   const { login } = useAuth();
-  const history = useHistory();
+  const navigate = useNavigate();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -20,7 +20,7 @@ export default function LoginForm() {
       setError("");
       setLoading(true);
       await login(email, password);
-      history.push("/");
+      navigate("/");
     } catch (err) {
       console.log(err);
       setLoading(false);
