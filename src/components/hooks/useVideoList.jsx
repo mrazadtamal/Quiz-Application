b4c2da271d45efd991600430d38b9f85677e5a1f
mrazadtamal <mrazadt@gmail.com>
2022-01-30 23:47:35 +0600

    firebase

---
+++ src/components/hooks/useVideoList.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from "react";
+
+const useVideoList = () => {
+  useEffect(() => {
+    async function fetchVideos() {}
+  }, []);
+
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default useVideoList;
