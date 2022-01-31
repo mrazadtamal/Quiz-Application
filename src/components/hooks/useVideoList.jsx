619fac3d2fbeae15b0f7dfbedbb413433ae29608
mrazadtamal <mrazadt@gmail.com>
2022-01-31 11:29:38 +0600

    firebase

--- src/components/hooks/useVideoList.jsx
+++ src/components/hooks/useVideoList.jsx
@@ -1,8 +1,24 @@
 import React, { useEffect } from "react";
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from "firebase/database";
 
 const useVideoList = () => {
   useEffect(() => {
-    async function fetchVideos() {}
+    async function fetchVideos() {
+      const db = getDatabase();
+
+      const videosRef = ref(db, "videos");
+      const videosQuery = query();
+    }
+
+    fetchVideos();
   }, []);
 
   return (
